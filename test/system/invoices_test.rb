ee6d7e2d4789b1fe571d7cc05669c61cfe2c24f0
Darrick Smith <smithwebdd@gmail.com>
2023-02-03 01:57:35 -0500

    [Test] Added additional tests for show, update, destroy actions

--- test/system/invoices_test.rb
+++ test/system/invoices_test.rb
@@ -1,25 +1,58 @@
 require "application_system_test_case"
 
 class InvoicesTest < ApplicationSystemTestCase
-  test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
-    visit quotes_path
+  setup do
+    @invoice = invoice(:first) # Reference to the first fixture invoice
+  end
+
+  test "Creating a new invoice" do
+    # When we visit the Invoices#index page
+    # we expect to see a title with the text "Invoices"
+    visit invoices_path
     assert_selector "h1", text: "Invoices"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
+    # When we click on the link with the text "New invoice"
+    # we expect to land on a page with the title "New invoice"
+    click_on "New invoice"
+    assert_selector "h1", text: "New invoice"
 
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
+    # When we fill in the name input with "Capybara invoice"
+    # and we click on "Create Invoice"
+    fill_in "Name", with: "Capybara invoice"
+    click_on "Create invoice"
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
+    # We expect to be back on the page with the title "Invoices"
+    # and to see our "Capybara invoice" added to the list
+    assert_selector "h1", text: "Invoices"
+    assert_text "Capybara invoice"
+  end
+
+  test "Showing an Invoice" do
+    visit invoices_path
+    click_link @invoice.name
+
+    assert_selector 'h1', test: @invoice.name
+  end
+
+  test "Updating an Invoice" do
+    visit invoices_path
+    assert_selector "h1", text: "Invoice"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit Invoice"
+
+    fill_in "Name", with: "Updated Invoice"
+    click_on "Update Invoice"
+
+    assert_selector "h1", text: "Invoice"
+    assert_text "Updated Invoice"
+  end
+
+  test "Destroying a quote" do
+    visit invoices_path
+    assert_text @invoice.name
+
+    click_on "Delete", match: :first
+    assert_no_text @invoice.name
   end
 end
