6499424d580e375d01813f97389c058fa91644c9
Darrick Smith <smithwebdd@gmail.com>
2023-02-03 22:28:37 -0500

    [Feature] Updated Invoices controller, Added resources route

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,46 @@
 class InvoicesController < ApplicationController
+  before_action :set_invoice, only: %i[show edit update destroy]
+
+  def index
+    @invoices = Invoice.all
+  end
+
+  def show; end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to invoices_path, notice: 'Invoice was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @invoice.update(invoice_params)
+      redirect_to invoices_path, notice: 'Invoice was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @invoice.destroy
+    redirect_to invoices_path, notice: 'Invoice was successfully destroyed.'
+  end
+
+  private
+
+  def set_invoice
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def invoice_params; end
 end
